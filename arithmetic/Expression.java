8a187c5fc5ef40f4450b12451a61055298840f64
AlinaMedvedva <a.medvedeva2@g.nsu.ru>
2024-10-27 17:37:57 +0700

    Update Expression.java

--- arithmetic/Expression.java
+++ arithmetic/Expression.java
@@ -2,29 +2,55 @@ package arithmetic;
 
 import java.util.HashSet;
 
-public abstract class Expression{
-    public String toString() {
-        return super.toString();
-    }
+public abstract class Expression implements Cloneable{
+
+    public Expression(){};
+    abstract public String toString();
 
     @Override
-    protected Expression clone() throws CloneNotSupportedException {
-        return null;
+    abstract protected  Expression clone() throws CloneNotSupportedException;
+
+    abstract public Expression derivative(String name);
+    public double calculate_expression(double num) throws MyException{
+        if(this.count_variable().size() > 1){
+            throw new MyException("Больше одной переменной");
+        }
+        return this.calculation(num);
     }
 
-    public Expression derivative(String name){
-        return null;
+    abstract public HashSet<Variable> count_variable();
+
+    abstract public double calculation(double num) ;
+
+    abstract public Expression calculation(String name, double num);
+
+    public Expression add(Expression e2){
+        return new Sum(this,e2);
     }
 
-    public HashSet<Variable> count_variable(){
-        return null;
+    public Expression mult(Expression e2){
+        return new Multiplication(this, e2);
     }
 
-    public double calculation(double num) throws MyException {
-        return Double.parseDouble(null);
+    public Expression difference(Expression e2){
+        return new Minus(this, e2);
     }
 
-    public Expression calculation(String name, double num){
-        return null;
+    public Expression div(Expression e2){
+        return new Splitting(this, e2);
+    }
+
+    public Expression pow(Expression e2){
+        return new Pow(this, e2);
+    }
+
+    public boolean isDouble(String s){
+        try{
+            Double p = Double.parseDouble(s);
+            return true;
+        }catch (NumberFormatException e)
+        {
+            return false;
+        }
     }
 }
